bebbee2b29c0da397e4ca4a9e76cedfdb8684cd4
AleksandroRD <ToriiPlay2016@gmail.com>
2024-03-15 17:11:43 +0100

    Added FPS controller

---
+++ Assets/Scripts/FirstPersonController.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+
+public class FirstPersonController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 2f;
+    [SerializeField]
+    private float sensitivity = 2f;
+    [SerializeField]
+    private CharacterController characterController;
+    private Camera playerCamera;
+    private float rotationX = 0f;
+
+    void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+        playerCamera = GetComponentInChildren<Camera>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        HandleMovement();
+        HandleMouseLook();
+    }
+
+    void HandleMovement()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, 0f, vertical) * speed * Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+
+        characterController.Move(movement);
+
+        // Simulate gravity
+        characterController.Move(Vector3.down * 9.8f * Time.deltaTime);
+    }
+
+    void HandleMouseLook()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity;
+
+        rotationX -= mouseY;
+        rotationX = Mathf.Clamp(rotationX, -90f, 90f);
+
+        playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0f, 0f);
+        transform.rotation *= Quaternion.Euler(0f, mouseX, 0f);
+    }
+
+}
