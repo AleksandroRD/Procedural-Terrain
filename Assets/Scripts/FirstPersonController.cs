c90a2e77536db0532105cf5eb33ad3b47e278837
AleksandroRD <ToriiPlay2016@gmail.com>
2024-07-19 16:01:52 +0200

    Updated FPS Controller

--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -3,17 +3,22 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public class FirstPersonController : MonoBehaviour
 {
-    [SerializeField] private float speed = 2f;
-    [SerializeField] private float sensitivity = 2f;
-    [SerializeField] private CharacterController characterController;
-    [SerializeField] private float weight = 60;
-    private Camera playerCamera;
-    private float rotationX = 0f;
+    [SerializeField] private float _walkingSpeed = 7.5f;
+    [SerializeField] private float _runningSpeed = 11.5f;
+    [SerializeField] private float _gravity = 20.0f;
+    [SerializeField] private float _cameraSensitivity = 2.0f;
+    [SerializeField] private float _maxPitch = 85;
+    [SerializeField] private float _minPitch = -90;
+
+    private CharacterController _characterController;
+    private float _rotationX = 0;
+
+    private Camera _playerCamera;
 
     void Awake()
     {
-        characterController = GetComponent<CharacterController>();
-        playerCamera = GetComponentInChildren<Camera>();
+        _characterController = GetComponent<CharacterController>();
+        _playerCamera = GetComponentInChildren<Camera>();
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
@@ -29,24 +34,26 @@ public class FirstPersonController : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
+        float speed = Input.GetKey(KeyCode.LeftShift) ? _runningSpeed : _walkingSpeed;
+        
         Vector3 movement = new Vector3(horizontal, 0f, vertical) * speed * Time.deltaTime;
         movement = transform.TransformDirection(movement);
 
-        characterController.Move(movement);
+        _characterController.Move(movement);
 
         // Simulate gravity
-        characterController.Move(Vector3.down * 9.8f * Time.deltaTime * weight);
+        _characterController.Move(Vector3.down * Time.deltaTime * _gravity);
     }
 
     void HandleMouseLook()
     {
-        float mouseX = Input.GetAxis("Mouse X") * sensitivity;
-        float mouseY = Input.GetAxis("Mouse Y") * sensitivity;
+        float mouseX = Input.GetAxis("Mouse X") * _cameraSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * _cameraSensitivity;
 
-        rotationX -= mouseY;
-        rotationX = Mathf.Clamp(rotationX, -90f, 90f);
+        _rotationX -= mouseY;
+        _rotationX = Mathf.Clamp(_rotationX, _minPitch, _maxPitch);
 
-        playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0f, 0f);
+        _playerCamera.transform.localRotation = Quaternion.Euler(_rotationX, 0f, 0f);
         transform.rotation *= Quaternion.Euler(0f, mouseX, 0f);
     }
 
